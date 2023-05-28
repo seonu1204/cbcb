12a60fe677bb8a483c1b40489972138e5e42227c
sy <dus1290@naver.com>
2023-05-28 20:24:40 +0900

    디비전까지 구현완

---
+++ board/src/main/java/com/example/board/entity/BoardFileEntity.java
@@ -0,0 +1,35 @@
+package com.example.board.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "board_file_table")
+public class BoardFileEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String originalFileName;
+
+    @Column
+    private String storedFileName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private BoardEntity boardEntity;
+
+    public static BoardFileEntity toBoardFileEntity(BoardEntity boardEntity, String originalFileName, String storedFileName) {
+        BoardFileEntity boardFileEntity = new BoardFileEntity();
+        boardFileEntity.setOriginalFileName(originalFileName);
+        boardFileEntity.setStoredFileName(storedFileName);
+        boardFileEntity.setBoardEntity(boardEntity); //pk값이 아니라 부모 엔티티 객체를 넘겨줌.
+        return boardFileEntity;
+    }
+}
